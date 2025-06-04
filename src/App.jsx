133551a0b012080ee5eec7d4e999095a80434eb1
Brandon <BrandonPilane@gmail.com>
2025-06-04 12:21:06 +0200

    feat:     ✨ Routes with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Favourites from "./pages/Favourites";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,8 +1,10 @@
 function Favourites() {
   return (
     <div className="favourites-empty">
-      <h1>No favourite movies yet</h1>
-      <p>Add movies to your 'favourites' and they will appear here!</p>
+      <h1>No favourite movies yet!</h1>
+      <p>Add movies to your 'favourites' and they will appear here.</p>
     </div>
   );
 }
+
+export default Favourites;
